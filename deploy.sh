7598e987d0afd66142d704c104cb30321b7c355c
Olivier Cervello <ocervello@freelabz.com>
2026-02-27 18:23:07 +0100

    remove deploy script

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-gcloud run deploy discord-webhook-forwarder \
-  --source . \
-  --set-env-vars DISCORD_WEBHOOK_URL=https://discord.com/api/webhooks/1407654919309230131/Vo5N9B7sfceSsQ-MYQWNGYiSw1PkZWNAv5ysjPidXo1dfNcpZH2j9yn3_XD3AxbXcBo4 \
-  --set-env-vars BASIC_AUTH_USERNAME=secator \
-  --set-env-vars BASIC_AUTH_PASSWORD=secator \
-  --allow-unauthenticated
